65039ddee1a82682354558d9f719a6dfd87b1f0d
Moritz Hanke <momo@MacBook-Air-von-Moritz.local>
2025-11-13 18:55:19 +0100

    adjusted path

--- R/instabilities_connis_SynechococcusPCC7942.R
+++ R/instabilities_connis_SynechococcusPCC7942.R
@@ -4,6 +4,8 @@ library(ConNIS)
 
 # Data from https://www.pnas.org/doi/full/10.1073/pnas.1519220112
 
+setwd("./R")
+
 data_t0 <- readxl::read_xlsx("./PCC7942/pnas.1519220112.sd01.xlsx")
 IS_pos_t0 <- sort(unique(data_t0$position))
 data_t6 <- readxl::read_xlsx("./PCC7942/pnas.1519220112.sd02.xlsx")
